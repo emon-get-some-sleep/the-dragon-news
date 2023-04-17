3e1118ca19799cbf0d3b81315e271013199f0aae
emonh0que <you@example.com>
2023-04-18 01:14:34 +0600

    let's build a newspaper webste baby

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDc5hSSTclLI8K1DCogaCPO-e2tiDyjf7o",
+  authDomain: "simple-firebase-8b219.firebaseapp.com",
+  projectId: "simple-firebase-8b219",
+  storageBucket: "simple-firebase-8b219.appspot.com",
+  messagingSenderId: "146991000177",
+  appId: "1:146991000177:web:8b601bbd38c6a237c909d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
